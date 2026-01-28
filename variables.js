c45969fe0d7e5cbb3338777dda0b0debf41cc8c7
mkersting <marian.kersting@gmail.com>
2026-01-28 02:50:40 +0100

    npm update

--- variables.js
+++ variables.js
@@ -1,7 +1,16 @@
 module.exports = function (self) {
 	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
+		{ variableId: 'selectedInput', name: 'Input to Switch.'},
+		{ variableId: 'selectedOutput', name: 'Output to Switch.'},
+		{ variableId: 'Input1Name', name: 'Label for Input 1.'},
+		{ variableId: 'Input2Name', name: 'Label for Input 2.'},
+		{ variableId: 'Input3Name', name: 'Label for Input 3.'},
+		{ variableId: 'Input4Name', name: 'Label for Input 4.'},
+		{ variableId: 'Output1Name', name: 'Label for Output 1.'},
+		{ variableId: 'Output2Name', name: 'Label for Output 2.'},
+		{ variableId: 'Output3Name', name: 'Label for Output 3.'},
+		{ variableId: 'Output4Name', name: 'Label for Output 4.'},
+		{ variableId: 'InputName', name: 'Label for Input so switch.'},
+		{ variableId: 'OutputName', name: 'Label for Output to switch.'},
 	])
 }
